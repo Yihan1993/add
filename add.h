5a039e773fa97fbf8c0d00ba8f831dad2802dfe0
Yihan CHENG FRD Z0010648 <yihan.cheng@zf.com>
2020-04-25 11:50:29 +0200

    a test c++ package for conan

---
+++ add.h
@@ -0,0 +1,7 @@
+#ifndef ADD_H
+#define ADD_H
+#include <iostream>
+
+int add(int a, int b);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+
+int main()
+{
+    int c =0;
+    c = add(1,2);
+    std::cout << c << std::endl;
+}
